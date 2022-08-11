4da5b543aa871b40b3c8e046aaa1716abd84b756
rubendramos <rubendramos@gmail.com>
2022-08-12 00:57:15 +0200

    Adding eureka and config server configuration

--- email-address/src/main/java/com/example/emailaddress/EmailaddressServiceApplication.java
+++ email-address/src/main/java/com/example/emailaddress/EmailaddressServiceApplication.java
@@ -2,9 +2,11 @@ package com.example.emailaddress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableTransactionManagement
 public class EmailaddressServiceApplication {
 
--- email-message/src/main/java/com/example/emailmessage/EmailmessageServiceApplication.java
+++ email-message/src/main/java/com/example/emailmessage/EmailmessageServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.emailmessage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmailmessageServiceApplication {
 
 	public static void main(String[] args) {
