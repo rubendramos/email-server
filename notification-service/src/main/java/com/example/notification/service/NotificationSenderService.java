9ee51364fd006bea96061023b72ef2f8428514e3
rubendramos <rubendramos@gmail.com>
2022-08-16 02:18:48 +0200

    Update notification-service

--- notification-service/src/main/java/com/example/notification/service/NotificationSenderService.java
+++ notification-service/src/main/java/com/example/notification/service/NotificationSenderService.java
@@ -1,5 +1,10 @@
 package com.example.notification.service;
 
+import com.example.notification.domain.Mail;
+
 public interface NotificationSenderService {
 
+	
+	public void sendNotification(Mail mail);
+	
 }
